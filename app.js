36ae36388c1ab77b60ec5019898d8599d35bc78a
Matis Rammi <matis.rammi@khk.ee>
2021-12-07 09:51:14 +0200

    Created Controllers and Data Structure

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
-// Module pattern basic structure
-(function(){
-    //Private variables and functions
-    return{
-       // Public variables and functions
+// Storage Controller
+// Create later
+
+// Item Controller
+const ItemCtrl = (function(){
+    // Item constructor
+    const Item = function(id, name, calories){
+        this.id = id
+        this.name = name
+        this.calories = calories
+    }
+
+    // Data Structure
+    const data= {
+        items: [
+            {id: 0, name:"Steak Dinner", calories: 1200},
+            {id: 1, name:"Cookie", calories: 400},
+            {id: 2, name:"Eggs", calories: 300}
+        ],
+        total: 0
+    }
+
+    return {
+        logData: function(){
+            return data
+        }
     }
 })();
 
 
+// UI Controller
+const UICtrl = (function(){
+    console.log("UICtrl")
+})();
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl){
+    return {
+        init: function(){
+            console.log("Initialiazing App")
+        }
+    }
+})(ItemCtrl, UICtrl);
+
+// Initialize App
+App.init()
