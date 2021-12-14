0d62520a738a2628827216644a21ce6714064610
Matis Rammi <matis.rammi@khk.ee>
2021-12-14 09:17:18 +0200

    Populated data srtucture items to list fix

--- app.js
+++ app.js
@@ -1,71 +1,55 @@
-// Storage Controller
-// Create later
+const ItemCtrl = (function () {
 
-// Item Controller
-const ItemCtrl = (function(){
-    // Item constructor
-    const Item = function(id, name, calories){
+    const Item = function (id, name, calories){
         this.id = id
         this.name = name
         this.calories = calories
     }
-
-    // Data Structure
-    const data= {
+    const data = {
         items: [
-            {id: 0, name:"Steak Dinner", calories: 1200},
-            {id: 1, name:"Cookie", calories: 400},
-            {id: 2, name:"Eggs", calories: 300}
+            {id: 0, name: "Steak Dinner", calories: 1200},
+            {id: 1, name: "Cookie", calories: 400},
+            {id: 2, name: "Eggs", calories: 300}
         ],
         total: 0
     }
 
     return {
-        getItems: function(){
+        getItems: function () {
             return data.items
         },
-        logData: function(){
+        logData: function (){
             return data
         }
     }
 })();
 
-
-// UI Controller
-const UICtrl = (function(){
+const UICtrl = (function () {
     return {
-        populateItemList: function (items){
-            // Create html content
-            let html = '';
+        populateItemList: function (items) {
+            let html = " ";
 
-            // Parse data and create list items html
-            items.forEach(function (item){
-                html += `<li class='collection-item' id='item ${item.id}'>
-                    <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
-                    <a href='#' class='secondary-content'>
-                        <i class='edit-item fa fa-pencil'></i>
-                    </a>
+            items.forEach(function (item) {
+                html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}:</strong><em>${item.calories} Calories</em>
+                <a herf="#" class="secondary-content">
+                <i class="edit-item fa fa-pencil"></i>
+                </a>
                 </li>`;
             });
-
-            // Insert list items
-            document.querySelector('#item-list').innerhtml = html;
+            document.querySelector("#item-list").innerHTML=html;
         }
     }
 })();
-
-// App Controller
-const App = (function(ItemCtrl, UICtrl){
-    return {
-        init: function(){
-            console.log("Initialiazing App")
-            // Fetch items from data structure
+const App = (function (ItemCtrl, UiCtrl) {
+    return{
+        init: function () {
+            console.log("Initializing App")
             const items = ItemCtrl.getItems()
-            // Populate items list
             UICtrl.populateItemList(items)
+            console.log(items)
         }
     }
-})(ItemCtrl, UICtrl);
+})(ItemCtrl,UICtrl);
 
-// Initialize App
 App.init()
